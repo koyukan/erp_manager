fc7782c8f5e1469fa5c9040f4725aad71d5feec0
Kaan <kaan@koyukan.com>
2024-08-23 14:55:04 +0300

    Fix cors

--- src/main.ts
+++ src/main.ts
@@ -32,7 +32,7 @@ async function bootstrap() {
   });
   // Enable CORS
   app.enableCors({
-    origin: ['http://localhost:3000'],
+    origin: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
